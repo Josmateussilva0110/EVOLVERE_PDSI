2e3d9893a1f52c473235a590ee1b558c3050a080
Jose Mateus <jmateussilva044@gmail.com>
2025-04-25 20:27:24 -0300

    feat: criei a tela de login

---
+++ frontend/lib/features/user/login_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: CadastroUsuarioScreen()));
+
+class CadastroUsuarioScreen extends StatefulWidget {
+  @override
+  _CadastroScreenState createState() => _CadastroScreenState();
+}
+
