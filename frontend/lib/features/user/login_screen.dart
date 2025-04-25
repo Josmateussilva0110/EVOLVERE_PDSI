9d45600c5156605132f6fde812f2499a4d3f723f
Lucaspm5 <lucasemanuelpm5.com>
2025-04-25 20:47:10 -0300

    feat: removi arquivo login

--- frontend/lib/features/user/login_screen.dart
+++ frontend/lib/features/user/login_screen.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp(home: CadastroUsuarioScreen()));
-
-class CadastroUsuarioScreen extends StatefulWidget {
-  @override
-  _CadastroScreenState createState() => _CadastroScreenState();
-}
-
+ 
+ 
+ 
+  
+   
+    
+     
+     
